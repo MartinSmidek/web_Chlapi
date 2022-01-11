71b27e62c5f171a4fda2f88851c8afea464fdf24
Martin Šmídek <martin@smidek.eu>
2022-01-11 12:15:40 +0100

    * CAC backup db

--- rr.inc.php
+++ rr.inc.php
@@ -19,9 +19,15 @@
       'activity'=>(object)array()
   );
 
+  // informace pro debugger o poloze ezer modulů
+  $dbg_info= (object)array(
+    'src_path'  => array('rr','man','ezer3.1') // poloha a preference zdrojových modulů
+  );
+
   // databáze
   $deep_root= "../files/chlapi";
   require_once("$deep_root/rr.dbs.php");
+  $path_backup= "$deep_root/sql";
 
   $app_php= array('rr/rr_fce.php');
   
